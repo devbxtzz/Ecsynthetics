1beb60dfdf028ae2e4ec0cc52aeb7a6a3a272f4a
Tonny Batya <84370752+devbxtzz@users.noreply.github.com>
2023-04-13 14:33:30 +0300

    Update hero.js

--- components/hero.js
+++ components/hero.js
@@ -9,7 +9,7 @@ export default function Hero() {
         <div className="flex items-center w-full lg:w-1/2">
           <div className="max-w-2xl mb-8">
             <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
-              <span className="text-blue-400">Ecsynths </span> a synthetic
+              <span className="text-blue-400">Ecsynths </span> is a synthetic
               derivatives DEX built on
               <span className="text-purple-700"> Glitch </span> DeFi protocol
             </h1>
